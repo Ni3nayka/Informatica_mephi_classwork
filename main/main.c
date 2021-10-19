c23cc1d49f831b41b3e3a29aee2d51c20cfe577b
Egor Bakay <egor_bakay@inbox.ru>
2021-10-19 10:03:30 +0300

    add work

--- main/main.c
+++ main/main.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void add(unsigned long int n, double *sum) {
+    *sum+=sqrt( (5+3*n) / ((double)(4+n)) );
+}
+
 int main() {
-    int a[3],x,y,x1,temp;
-    scanf("%i %i %i", &a[0], &a[1], &a[2]);
-    scanf("%i %i", &x, &y);
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3-i; j++) {
-            if (a[j]>a[j+1]) {
-                temp=a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
-            }
-        }
+    double sum = 34640983.186792,S=34640984.919544;//0.0;
+    //for (unsigned long int N=1; N<20000000; N++) add(N,&sum);
+    //printf("%f\n",sum);
+    for (unsigned long int N=20000000; N<=200000000; N+=20000000) {
+        add(N,&sum);
+        printf("%9i%18.4f%18.4f%10.4f\n",N,sum,S,sum-S);
+
     }
-    if (a[0]==a[1]&&a[1]==a[2]) printf("equal\n");
-    else {
-        if (a[1]==a[2]) printf("%i two\n",a[2]);
-        else printf("%i\n",a[2]);
-    }
-    x1 = sqrt(abs(250*250-y*y));
-    x = abs(x);
-    if (x<250&&x>0 && (y>0&&y<250&&x>x1 || y<0&&y>-250&&x<x1&&x>y+250)) printf("inside\n");
-    else printf("outside\n");
     return 0;
 }
