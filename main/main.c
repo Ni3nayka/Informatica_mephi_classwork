eb1e78ad0e0900db2c92f762faabcdf33d6c4e1c
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-10-19 23:41:22 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,31 +2,21 @@
 #include <stdlib.h>
 
 double summand(unsigned long int n) {
-    return sqrt( (5+3*n) / ((double)(4+n)) );
+    return ((double)1)/(n*(n+1)) ;
 }
 
-void add_2(double n, double *S, double *fcor, double *cor, double *Scor) {
-    for (unsigned long int N=n-20000000+1; N<=n; N++) {
-        *fcor = summand(N) - *cor;
-        *Scor = *S + *fcor;
-        *cor = (*Scor - *S) - *fcor;
-        *S = *Scor;
-    }
-}
-
-void add_1(double n, double *S) {
-    for (unsigned long int N=n-20000000+1; N<=n; N++) {
+void add(int n, double *S) {
+    for (int N=1; N<=n; N++) {
         *S += summand(N);
+        //printf("%i %0.16f\n",N,summand(N));
     }
 }
 
 int main() {
     double sum = 0.0;
-    double S = 0.0, fcor = 0.0, cor = 0.0, Scor = 0.0;
-    for (unsigned long int N=20000000; N<=200000000; N+=20000000) {
-        add_1(N, &sum);
-        add_2(N, &S, &fcor, &cor, &Scor);
-        printf("%9i%18.4f%18.4f%10.4f\n",N,sum,S,sum-S);
-    }
+    int n = 0;
+    scanf("%i", &n);
+    add(n,&sum);
+    printf("%.8f\n%.8f\n%.8f\n",1.0,sum,1.0-sum);
     return 0;
 }
