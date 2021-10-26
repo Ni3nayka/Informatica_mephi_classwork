d30c98ad390b9b8caad6fcf634222cdf76c6848c
Egor Bakay <egor_bakay@inbox.ru>
2021-10-26 09:31:54 +0300

    Create lab.c

---
+++ old_task/6/lab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+double function(double x) {
+    return x-log(x)-2;
+}
+
+double abs1(double x) {
+    return x>0?x:-x;
+}
+
+int main() {
+    double a=1.0,b=5.0,c=0.0;
+    while (abs1(function((a+b)/2))>0.00000001) {
+        c = (a+b)/2;
+        if (function(c)<0) a = c;
+        else b = c;
+    }
+    printf("%1.8f\n",(a+b)/2);
+    return 0;
+}
