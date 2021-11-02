3e815480fa77cc6bc652dac771541a43e6f2a90d
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-11-02 11:33:52 +0300

    finish all debt

--- main/main.c
+++ main/main.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-double function(double x) {
-    return sin(x)-2.0/5.0;
+double summand(double n) {
+    return ((double)1)/(n*(n+1)) ;
 }
 
-double abs1(double x) {
-    return x>0?x:-x;
-}
-
-double add(double xs, double x, double n) {
-    return (-1)*xs*x*x/((double)(n*(n-1)));
+void add(double n, double *S) {
+    for (double N=1.0; N<=n; N+=1.0) {
+        *S += summand(N);
+    }
 }
 
 int main() {
-    double a=2.0,b=4.0,c=0.0,x0=0.0;
-    while (abs1(function((a+b)/2))>0.0000001) {
-        c = (a+b)/2.0;
-        if (function(c)>0) a = c;
-        else b = c;
-    }
-    x0 = (a+b)/2;
-    printf("%1.7f\n",x0);
-    double sum = 0.0,xn=0.0;
-    xn=x0; sum=x0;
-    for (int i=3;i<200;i+=2) {
-        xn = add(xn,x0,i);
-        sum += xn;
-    }
-    printf("%1.6f\n",sum);
+    double sum = 0.0;
+    int n = 0;
+    scanf("%i", &n);
+    add((double)n,&sum);
+    printf("%.8f\n%.8f\n%.8f\n",1.0,sum,1.0-sum);
     return 0;
 }
--- old_task/5/sr.c
+++ old_task/5/sr.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double summand(unsigned long int n) {
+double summand(double n) {
     return ((double)1)/(n*(n+1)) ;
 }
 
-void add(int n, double *S) {
-    for (int N=1; N<=n; N++) {
+void add(double n, double *S) {
+    for (double N=1.0; N<=n; N+=1.0) {
         *S += summand(N);
-        //printf("%i %0.16f\n",N,summand(N));
     }
 }
 
@@ -16,7 +15,7 @@ int main() {
     double sum = 0.0;
     int n = 0;
     scanf("%i", &n);
-    add(n,&sum);
+    add((double)n,&sum);
     printf("%.8f\n%.8f\n%.8f\n",1.0,sum,1.0-sum);
     return 0;
 }
