371c89001fc62cc6fea35fa9a878cef738bc4990
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-11-16 08:56:53 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -3,38 +3,10 @@
 #include <math.h>
 #define len 40
 int main() {
-    int array[len] = {0}, m=0, a=5, count=0;
-    /*int range_min=0, range_max=0;
-    scanf("%i", &range_min);
-    scanf("%i", &range_max);
-    for (int i=0; i<len; i++) array[i] = rand()%(range_max-range_min+1)+range_min;
-    for (int i=0; i<len; i++) printf("%i %i\n", i+1, array[i]);*/
-    for (int i=0; i<len; i++) scanf("%i", &array[i]);
-    scanf("%i", &a);
-    for (int i=0; i<len; i++)
-        if (array[i]>a) {
-            m += array[i];
-            count++;
-        }
-    printf("%.4f\n",(double)m/(double)count);
-    m/=count;
-    count = 0;
-    for (int i=0; i<len; i++)
-        if (array[i]>m) count++;
-    printf("%i\n",count);
-    /*int *p; p = array;
-    for (int i=0; i<len; i++) scanf("%i", &(*(p+i)));
-    scanf("%i", &a);
-    for (int i=0; i<len; i++)
-        if (*(p+i)>a) {
-            m += *(p+i);
-            count++;
-        }
-    printf("%.4f\n",(double)m/(double)count);
-    m/=count;
-    count = 0;
-    for (int i=0; i<len; i++)
-        if (*(p+i)>m) count++;
-    printf("%i\n",count);*/
+    int i, m[7], x;
+    for (i=0; i<7; ++i)
+        m[i]=i/3+1;
+    x = m[m[2]+m[6]];
+    printf("%d ", x);//*(m+i)-*(m+i-1));
     return 0;
 }
