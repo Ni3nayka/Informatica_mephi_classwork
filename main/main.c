fab9f93bf0eb4b914937512d63c3c11d77c665f9
Egor Bakay <egor_bakay@inbox.ru>
2021-11-23 14:09:50 +0300

    add classwork

--- main/main.c
+++ main/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define len 40
+
+#define S(h,a) (double)(a*3*h)
+#define S_o(a) ((double)3*sqrt((double)3)/(double)2*(double)a*(double)a)
+#define V(h,a) ((double)1/(double)3*(double)S_o(a)*(double)h)
+#define abs(a) (a>1.0?(double)a-1.0:1.0-a)
+
 int main() {
-    int i, m[7], x;
-    for (i=0; i<7; ++i)
-        m[i]=i/3+1;
-    x = m[m[2]+m[6]];
-    printf("%d ", x);//*(m+i)-*(m+i-1));
+    int H=0,A=0;
+    double D=100.0,f=0.0;
+    for (int h=1;h<=50;h++)
+        for (int a=1;a<=50;a++) {
+            f = V(h,a)/S(h,a);
+            if (D>abs(f)) {
+                D = abs(f);
+                H = h;
+                A = a;
+            }
+        }
+    printf("%i\n%i\n",H,A);
     return 0;
 }
