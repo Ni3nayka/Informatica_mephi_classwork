2ae5f65a61c0fbc02cd337bb0130f56e6805775c
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-12-14 00:46:26 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,25 +2,51 @@
 #include <stdlib.h>
 #include <math.h>
 
-long int F(long int n_last_1, long int n_last_2, int i) {
-    long int t = n_last_1+n_last_2;
-    // comment - the requirement of the second task
-    //printf("%i) %i %i %i\n",i,n_last_1,n_last_2,t);
-    if (t<n_last_2) return 0;
-    else if (i==0) return t;
-    else return F(n_last_2,t,i-1);
-
+long int convect_to_10(long int t) {
+    if (t==0) return 0;
+    int znak = t>0?1:-1;
+    long int answer = 0;
+    t *= znak;
+    for (int i = 0; t>0; i++) {
+        answer += (t%10)*pow(5,i);
+        t /= 10;
+    }
+    answer *= znak;
+    return answer;
 }
 
-int main() {
-    long int n=0;
-    scanf("%i",&n);
-    if (n==1) printf("1\n");
-    else if (n==2) printf("3\n");
-    else {
-        n = F(1,3,n-3);
-        if (n==0) printf("!\n");
-        else printf("%i\n",n);
+
+
+long int convect_to_5(long int t, int *test) {
+    if (t==0) return 0;
+    int znak = t>0?1:-1;
+    long int answer = 0;
+    t *= znak;
+    if (t>4687499) { *test = 0; return 0; }
+    for (int i = 0; t>0; i++) {
+        answer += t%5*pow(10,i);
+        printf("%li %li %li\n",t%5,(long int)pow(10,i),t%5*((long int)pow(10,i)));
+        t /= 5;
     }
+    /*t = answer;
+    answer = 0;
+    while (t>0) {
+        answer = answer*10 + t%10;
+        t /= 10;
+    }*/
+    answer *= znak;
+    return answer;
+}
+
+
+int main() {
+    long int a = 0, b = 0;
+    int test = 1;
+    scanf("%li", &a);
+    scanf("%li", &b);
+    printf("%li\n",convect_to_10(a));
+    b = convect_to_5(b,&test);
+    if (test) printf("%li\n",b);
+    else printf("%!\n");
     return 0;
 }
