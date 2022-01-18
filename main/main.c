7997019dee72ebe90081be5b618a86e7cebb6700
Egor Bakay <egor_bakay@inbox.ru>
2022-01-18 13:01:21 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,50 +2,131 @@
 #include <stdlib.h>
 #include <math.h>
 
-long int pow1(long int a, long int b) {
+int ERROR = 0;
+const long int MAX_NUMBER = 2147483647;
+const long int MIN_NUMBER = -2147483648;
+
+long my_plus(long int si_a, long int si_b) {
+    if (ERROR) {
+        return 0;
+    }
+    if (((si_b > 0) && (si_a > (MAX_NUMBER - si_b))) || ((si_b < 0) && (si_a < (MIN_NUMBER - si_b)))) {
+        ERROR = 1;
+        return 0;
+    }
+    else {
+        return si_a + si_b;
+    }
+}
+
+long int my_minus(long int si_a, long int si_b) {
+    if (ERROR) {
+        return 0;
+    }
+    if ((si_b > 0 && si_a < MIN_NUMBER + si_b) || (si_b < 0 && si_a > MAX_NUMBER + si_b)) {
+        ERROR = 2;
+        return 0;
+    }
+    else {
+        return si_a - si_b;
+    }
+}
+
+long int my_multiply(long int si_a, long int si_b) {
+    if (ERROR) {
+        return 0;
+    }
+    if (si_a > 0) {
+        if (si_b > 0) {
+            if (si_a > (MAX_NUMBER / si_b)) {
+                ERROR = 3;
+            }
+        }
+        else {
+            if (si_b < (MIN_NUMBER / si_a)) {
+                ERROR = 3;
+            }
+        }
+    }
+    else {
+        if (si_b > 0) {
+            if (si_a < (MIN_NUMBER / si_b)) {
+                ERROR = 3;
+            }
+        }
+        else {
+            if ( (si_a != 0) && (si_b < (MAX_NUMBER / si_a))) {
+                ERROR = 3;
+            }
+        }
+    }
+
+    if (ERROR) {
+        return 0;
+    }
+    else {
+        return si_a * si_b;
+    }
+}
+
+long int my_pow(long int a, long int b) {
+    if (ERROR) return 0;
     if (b==0) return 1;
-    else if (b==1) return a;
+    else if (b==1) {
+        return a;
+    }
     long int answer = a;
-    for (;b>1;b--) answer *= a;
+    for (;b>1&&ERROR==0;b--) {
+        answer = my_multiply(answer,a);
+    }
+    if (ERROR) {
+        ERROR = 4;
+    }
     return answer;
 }
 
-long int convect_to_10(long int t) {
-    if (t==0) return 0;
-    int znak = t>0?1:-1;
-    long int answer = 0;
-    t *= znak;
-    for (long int i = 0; t>0; i++) {
-        answer += (t%10)*pow1(5,i);
-        t /= 10;
+long int my_div(long int a, long int b) {
+    if (ERROR) return 0;
+    if (b==0) {
+        ERROR = 5;
+        return 0;
     }
-    answer *= znak;
-    return answer;
+    return a/b;
 }
 
-int convect_to_5(long int t, long int *answer) {
-    if (t==0) return 0;
-    int znak = t>0?1:-1;
-    *answer = 0;
-    t *= znak;
-    if (t>4687499 || t<-4687499) { *answer = 0; return 0; }
-    for (long int i = 0; t>0; i++) {
-        *answer += t%5*pow1(10,i);
-        t /= 5;
+long int my_mod(long int a, long int b) {
+    if (ERROR) return 0;
+    if (b==0) {
+        ERROR = 6;
+        return 0;
     }
-    *answer *= znak;
-    return 1;
+    return a-(a/b)*b;
 }
 
+long int function_1(long int a, long int b) {
+    return my_plus(my_minus(my_multiply(my_pow(a,2),b),my_multiply(b,my_minus(b,a))),b);
+}
+
+long int function_2(long int a, long int b) {
+    return my_div(my_plus(my_pow(a,3),b),b);
+}
+
+long int function_3(long int a, long int b) {
+    return my_mod(my_plus(a,my_pow(b,3)),a);
+}
+
+void print(long int a) {
+    if (ERROR) printf("!%i\n",ERROR);
+    else printf("%li\n",a);
+    ERROR = 0;
+}
 
 int main() {
     long int a = 0, b = 0;
-    int test = 1;
     scanf("%li", &a);
     scanf("%li", &b);
-    printf("%li\n",convect_to_10(a));
-    test = convect_to_5(b,&b);
-    if (test) printf("%li\n",b);
-    else printf("%!\n");
+    print(function_1(a,b));
+    print(function_2(a,b));
+    print(function_3(a,b));
     return 0;
 }
