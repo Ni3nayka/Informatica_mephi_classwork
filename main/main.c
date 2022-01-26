cd334821c040f153132e213e3e77fd7a07ee0128
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-01-26 19:36:12 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
-float test(int A[6], int n) {
-    int sum = 0, count = 0;
-    for (int i = 0; i < 6; i++) {
-        if (A[i]<=A[n]) {
-            count++;
-            sum += A[i];
-        }
-    }
-    return (float)sum/count;
-}
-
 int main() {
-    int A[6] = {0};
-    for (int i = 0; i < 6; i++) {
+    const int N = 10;
+    int A[10] = {0};
+    int count = 0;
+    for (int i = 0; i<10; i++) {
         scanf("%i",&A[i]);
     }
-    for (int i = 0; i < 6; i++) {
-        printf("%6.2f",test(A,i));
+    for (int i = 0; i<10; i++) {
+        count = 0;
+        for (int u = 0; u<10; u++) {
+            if (A[u]==A[i]) {
+                count++;
+            }
+        }
+        if (count==1) {
+            printf("%4i",A[i]);
+        }
+        else {
+            printf("   0");
+        }
     }
     printf("\n");
+    return 0;
 }
