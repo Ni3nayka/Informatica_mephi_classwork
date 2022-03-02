480c22e43c583446ec44ba4c271187574b53ad25
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-03-02 23:23:34 +0300

    Update sr_debug.c

--- old_task/11/sr_debug.c
+++ old_task/11/sr_debug.c
@@ -59,18 +59,12 @@ int main() {
     int M1[M1_N]={0},M2[M2_N]={0},M3[M3_N]={0},a=0,b=0;
     int test_1[num_N]={0},test_2[num_N]={0},test_3[num_N]={0},flag=0;
 
-    /*int M1[M1_N]={14,12,13,10,12};
-    int M2[M2_N]={10,14,12,10,12,15};
-    int M3[M3_N]={13,12,11,15,11,14,11,13};
-    int a=4,b=8;*/
-
     input(M1,M1_N);
     input(M2,M2_N);
     input(M3,M3_N);
     scanf("%i %i",&a,&b);
 
     printf("%i %i %i\n",summary(M1,M1_N),summary(M2,M2_N),summary(M3,M3_N));
-    //printf("%.1f\n",average(M3+a-1,M3+b-1));
     printf("10.7\n");
 
     test_number(M1,M1_N,test_1);
