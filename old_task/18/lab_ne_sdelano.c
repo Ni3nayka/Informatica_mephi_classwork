64358d33b71651ace527acd58e918c8de3c917ec
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-06-16 01:14:38 +0300

    +

--- old_task/18/lab_ne_sdelano.c
+++ old_task/18/lab_ne_sdelano.c
@@ -3,53 +3,93 @@
 
 #define DEBUG 0
 
-//============================ String ========================================
-
-struct String {
-    char *data;
-    int size;
-};
-
-struct String mem_1(n) {
-
-}
-
-//============================ Vec ========================================
-
-struct Vec {
-    int *data;
-    int size;
-};
-
-//============================ main ========================================
+#define SIZE 12
 
 struct one_data {
-    struct String name;
-    struct Vec data;
+    char name[15];
+    int cpu,hdd,system_po,work_po;
 };
 
-int *read(int *n) {
-    /*int *array;
-    if (DEBUG) {
-        *n = rand()%3 + 3;
-        array = mem((*n)*(*n));
-        for (int i=0; i<(*n)*(*n); ++i) {
-            array[i] = rand()%10;
-        }
-    }
-    else {
-        scanf("%d", n);
-        array = mem((*n)*(*n));
-        input(array, (*n)*(*n));
-    }
-    return array;*/
-}
 
 int main() {
-    char a[2][3] = {1,2,3,4,5,6};
+    int size = 0;
+    char buffer[40];
+    int counter = 0;
 
-    printf("%i\n",a[1]);
-    printf("%i\n",*a+3);
+    struct one_data array[SIZE];
+    for (int i = 0; i<SIZE; i++) {
+        for (int u = 0; u<15; u++) {
+            array[i].name[u] = ' ';
+        }
+        array[i].cpu = 0;
+        array[i].hdd = 0;
+        array[i].system_po = 0;
+        array[i].work_po = 0;
+    }
+
+    scanf("%d",&size);
+    if (DEBUG) {
+        printf("size: %i\n",size);
+    }
+
+    gets(buffer);
+    int flag = 1;
+    for (int i = 0; i<size; i++) {
+        gets(buffer);
+        counter = 0;
+        flag = 1;
+        for (int u = 0; u<40&&counter<5; u++) {
+            if (buffer[u]==' ' || buffer[u]==0) {
+                counter++;
+                flag = 1;
+            }
+            else if (counter==0) {
+                array[i].name[u] = buffer[u];
+            }
+            else if (counter==1) {
+                if (flag) {
+                    flag = 0;
+                    array[i].cpu = array[i].cpu*10+(int)atoi(&buffer[u]);
+                }
+            }
+            else if (counter==2) {
+                if (flag) {
+                    flag = 0;
+                    array[i].hdd = array[i].hdd*10+(int)atoi(&buffer[u]);
+                }
+            }
+            else if (counter==3) {
+                if (flag) {
+                    flag = 0;
+                    array[i].system_po = array[i].system_po*10+(int)atoi(&buffer[u]);
+                }
+            }
+            else if (counter==4) {
+                if (flag) {
+                    flag = 0;
+                    array[i].work_po = array[i].work_po*10+(int)atoi(&buffer[u]);
+                }
+            }
+        }
+    }
+
+    if (DEBUG) {
+        printf("================\n");
+    }
+
+    for (int i = 0; i<size; i++) {
+        for (int u = 0; u<15; u++) {
+            printf("%c",array[i].name[u]);
+        }
+        printf("%5i",array[i].cpu);
+        printf("%5i",array[i].hdd);
+        printf("%5i",array[i].system_po);
+        printf("%5i",array[i].work_po);
+        printf("\n");
+    }
+    printf("\n");
+
+    printf("\n");
 
     return 0;
 }
